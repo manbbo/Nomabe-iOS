d8a52989d1a06470aa47842bbdae1754f010c124
elpitohn <11895336+elpitohn@users.noreply.github.com>
2024-07-01 19:57:48 -0300

    Put mocked there

--- Nomabe iOS/talkwithai/presentation/TalkWithAIScreenView.swift
+++ Nomabe iOS/talkwithai/presentation/TalkWithAIScreenView.swift
@@ -12,22 +12,8 @@ struct TalkWithAIScreenView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var isDisabled = true
     
-    @State var chatHist = [
-        MessageModel(messageId: 0, sentMessage: true, content: "Olá!"),
-        MessageModel(messageId: 1, sentMessage: false, content: "Oi! Como posso ajudar hoje?"),
-        MessageModel(messageId: 2, sentMessage: true, content: "Estou procurando um prato saudável para a minha dieta. Você pode me ajudar?"),
-        MessageModel(messageId: 3, sentMessage: false, content: "Claro! Temos várias opções saudáveis. Você tem alguma preferência?"),
-        MessageModel(messageId: 4, sentMessage: true, content: "Prefiro algo com frango ou peixe."),
-        MessageModel(messageId: 5, sentMessage: false, content: "Ótimo! Aqui estão algumas sugestões:\n1. Frango Grelhado com Legumes\n2. Salmão com Aspargos\n3. Ceviche de Peixe\nQual deles você gostaria de saber mais?"),
-        MessageModel(messageId: 6, sentMessage: true, content: "Me fale mais sobre o Frango Grelhado com Legumes."),
-        MessageModel(messageId: 7, sentMessage: false, content: "O Frango Grelhado com Legumes é delicioso e saudável. Ele vem com uma seleção de legumes frescos. Tem 250 calorias e 40g de proteínas. Gostaria de pedir este prato?"),
-        MessageModel(messageId: 8, sentMessage: true, content: "Sim, parece perfeito! Vou querer esse."),
-        MessageModel(messageId: 9, sentMessage: false, content: "Ótima escolha! Seu pedido de Frango Grelhado com Legumes foi confirmado. Algo mais que eu possa ajudar?"),
-        MessageModel(messageId: 10, sentMessage: true, content: "Não, obrigado!"),
-        MessageModel(messageId: 11, sentMessage: false, content: "De nada! Tenha um ótimo dia!")
-    ]
+    @State var chatHist = mockedChatHist
 
-    
     var body: some View {
         NavigationStack{
             VStack{
